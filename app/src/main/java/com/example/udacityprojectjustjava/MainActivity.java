c8a35d969841a8e5c301e39a17b4e5828a68d5f9
Fakhriddin <tanto.fn@gmail.com>
2021-11-07 16:10:27 +0500

    Just Java

--- app/src/main/java/com/example/udacityprojectjustjava/MainActivity.java
+++ app/src/main/java/com/example/udacityprojectjustjava/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.udacityprojectjustjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
